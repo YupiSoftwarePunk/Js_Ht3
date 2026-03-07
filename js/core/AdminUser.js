76e830c8264604bc8d996bab77e30822a41b1791
Yupi <youngperchik2404@gmail.com>
2026-03-07 12:55:12 +0500

    started base structere and it needs implementation

---
+++ js/core/AdminUser.js
@@ -0,0 +1,34 @@
+class AdminUser extends User {
+    #permissions;
+    static MAX_PERMISSIONS;
+
+    grantPermission(permission)
+    {
+
+    }
+
+    revokePermission(permission)
+    {
+
+    }
+
+    hasRole()
+    {
+
+    }
+
+    getPermissions()
+    {
+
+    }
+
+    canManageUsers()
+    {
+
+    }
+
+    banUser(userId, reason)
+    {
+        
+    }
+}
